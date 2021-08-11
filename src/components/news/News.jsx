d34ae6d6f3bd98f43d668515106f3c2fabef0a39
John Tran <johntran@Johns-MacBook-Air.local>
2021-08-11 00:32:08 -0700

    News banner

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -9,9 +9,7 @@ export default function News() {
   return (
     <>
       <Navbar />
-      <HeroImageNews src="https://static.wixstatic.com/media/056a2c_eb045b460b1c4b1dbb93fd80d5299e99~mv2_d_3840_2160_s_2.png/v1/crop/x_0,y_111,w_3840,h_1115/fill/w_2560,h_682,al_c,q_90,usm_0.66_1.00_0.01/RCM%201X%20CNG%20EnerSea%20OCEAN_0003.webp">
-        NEWS
-      </HeroImageNews>
+      <HeroImageNews src="./news.png" />
       <Container>
         <Row className="pt-5">
           <Col sm={12} md={8} className="pr-lg-5">
