ff7b1610d39974c41fc52ac897fd831b688cba24
John Tran <johntran@Johns-MacBook-Air.local>
2021-08-11 00:27:17 -0700

    Privacy and terms update

--- src/components/contact/DescriptionPrivacy.jsx
+++ src/components/contact/DescriptionPrivacy.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import { Form } from 'react-bootstrap';
 import SectionTitle from '../shared/text/SectionTitle';
 import SectionText from '../shared/text/SectionText';
 
@@ -8,33 +7,109 @@ export default function DescriptionPrivacy() {
   return (
     <section>
       <center>
-        <SectionTitle>Get in touch with the EnerSea Team</SectionTitle>
+        <SectionTitle>Privacy Policy of EnerSea Transport Inc.</SectionTitle>
       </center>
-
-      <Form>
-        Contact Info here
-        <label type="text">Name</label>
-      </Form>
-
       <SectionText className="mt-3">
-        EnerSea delivers the complete ”gas supply to energy market” solution for clients with
-        stranded gas resources or remote energy markets. To achieve this, EnerSea provides project
-        development and execution services to integrate its proprietary VOTRANS™ CNG system with
-        onshore/offshore gas supply and delivery terminals and power plants where required.
-      </SectionText>
-      <SectionText>
-        There are over 100 trillion cubic feet of stranded gas resources and curtailed production
-        globally, which currently provide no value to their asset owners. In addition, there is a
-        worldwide demand for power projects using natural gas instead of liquid fuels to ensure
-        long-term, low-cost fuel supplies, while reducing carbon emissions by more than 50%.
-        Deployment of the industry leading VOTRANS™ CNG system enables monetization of these gas
-        resources and stable fuel supply for power projects, where LNG or pipelines are not viable
-        options due to economic, geopolitical or environmental issues.
-      </SectionText>
-      <SectionText>
-        EnerSea’s marine gas transportation services are offered on a long-term tariff basis similar
-        to third-party pipeline transportation agreements. This ensures clients are not burdened
-        with the upfront capital expenditures related to the dedicated CNG ships or terminals.
+        <br />
+        <br />
+        <b>Introduction</b>
+        <br />
+        <br />
+        EnerSea Transport Inc. operates the www.EnerSea.com website, which provides the SERVICE.
+        This page is used to inform website visitors regarding our policies with the collection,
+        use, and disclosure of Personal Information if anyone decided to use our Service, the
+        EnerSea Transport website. If you choose to use our Service, then you agree to the
+        collection and use of information in relation with this policy. The Personal Information
+        that we collect are used for providing and improving the Service. We will not use or share
+        your information with anyone except as described in this Privacy Policy. The terms used in
+        this Privacy Policy have the same meanings as in our Terms and Conditions, which is
+        accessible at www.EnerSea.com, unless otherwise defined in this Privacy Policy. Our Privacy
+        Policy was created with the help of the Privacy Policy Template.
+        <br />
+        <br />
+        <b>Information Collection and Use</b>
+        <br />
+        <br />
+        For a better experience while using our Service, we may require you to provide us with
+        certain personally identifiable information, including but not limited to your name, phone
+        number, and postal address. The information that we collect will be used to contact or
+        identify you.
+        <br />
+        <br />
+        <b>Log Data</b>
+        <br />
+        <br />
+        We want to inform you that whenever you visit our Service, we collect information that your
+        browser sends to us that is called Log Data. This Log Data may include information such as
+        your computer’s Internet Protocol (IP) address, browser version, pages of our Service that
+        you visit, the time and date of your visit, the time spent on those pages, and other
+        statistics.
+        <br />
+        <br />
+        <b>Cookies</b>
+        <br />
+        <br />
+        Cookies are files with small amount of data that is commonly used an anonymous unique
+        identifier. These are sent to your browser from the website that you visit and are stored on
+        your computer’s hard drive. Our website uses these cookies to collection information and
+        improve our Service. You have the option to either accept or refuse these cookies, and know
+        know when a cookie is being sent to your computer. If you choose to refuse our cookies, you
+        may not be able to use some portions of our Service.
+        <br />
+        <br />
+        <b>Service Providers</b>
+        <br />
+        <br />
+        We may employ third-party companies and individuals due to the following reasons:
+        <ul>
+          <li>To facilitate our Service;</li>
+          <li>To provide the Service on our behalf;</li>
+          <li>To perform Service-related services; or</li>
+          <li>To assist us in analyzing how our Service is used.</li>
+        </ul>
+        <br />
+        <br />
+        We want to inform our Service users that these third parties have access to your Personal
+        Information. The reason is to perform the tasks assigned to them on our behalf. However,
+        they are obligated not to disclose or use the information for any other purpose.
+        <br />
+        <br />
+        <b>Security</b>
+        <br />
+        <br />
+        We value your trust in providing us your Personal Information, thus we are striving to use
+        commercially acceptable means of protecting it. But remember that no method of transmission
+        over the internet, or method of electronic storage is 100% secure and reliable, and we
+        cannot guarantee its absolute security.
+        <br />
+        <br />
+        <b>Links to Other Sites</b>
+        <br />
+        <br />
+        Our Service may contain links to other sites. If you click on a third-party link, you will
+        be directed to that site. Note that these external sites are not operated by us. Therefore,
+        we strongly advise you to review the Privacy Policy of these websites. We have no control
+        over, and assume no responsibility for the content, privacy policies, or practices of any
+        third-party sites or services.
+        <br />
+        <br />
+        <b>Changes to This Privacy Policy</b>
+        <br />
+        <br />
+        We may update our Privacy Policy from time to time. Thus, we advise you to review this page
+        periodically for any changes. We will notify you of any changes by posting the new Privacy
+        Policy on this page. These changes are effective immediately, after they are posted on this
+        page.
+        <br />
+        <br />
+        <b>Contact Us</b>
+        <br />
+        <br />
+        If you have any questions or suggestions about our Privacy Policy, do not hesitate to
+        contact us. Links with contact information have been provided using the navigation bar above
+        or listed below.
+        <br />
+        <br />
       </SectionText>
     </section>
   );
--- src/components/contact/DescriptionTerms.jsx
+++ src/components/contact/DescriptionTerms.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
-import { Form } from 'react-bootstrap';
 import SectionTitle from '../shared/text/SectionTitle';
 import SectionText from '../shared/text/SectionText';
 
@@ -8,33 +7,130 @@ export default function DescriptionTerms() {
   return (
     <section>
       <center>
-        <SectionTitle>Get in touch with the EnerSea Team</SectionTitle>
+        <SectionTitle>Terms and Conditions of EnerSea Transport Inc.</SectionTitle>
       </center>
-
-      <Form>
-        Contact Info here
-        <label type="text">Name</label>
-      </Form>
-
+      <b>Introduction</b>
       <SectionText className="mt-3">
-        EnerSea delivers the complete ”gas supply to energy market” solution for clients with
-        stranded gas resources or remote energy markets. To achieve this, EnerSea provides project
-        development and execution services to integrate its proprietary VOTRANS™ CNG system with
-        onshore/offshore gas supply and delivery terminals and power plants where required.
+        These Website Standard Terms and Conditions written on this webpage shall manage your use of
+        our website, EnerSea Transport accessible at www.EnerSea.com. These Terms will be applied
+        fully and affect to your use of this Website. By using this Website, you agreed to accept
+        all terms and conditions written in here. You must not use this Website if you disagree with
+        any of these Website Standard Terms and Conditions.
       </SectionText>
+      <b>Intellectual Property Rights</b>
       <SectionText>
-        There are over 100 trillion cubic feet of stranded gas resources and curtailed production
-        globally, which currently provide no value to their asset owners. In addition, there is a
-        worldwide demand for power projects using natural gas instead of liquid fuels to ensure
-        long-term, low-cost fuel supplies, while reducing carbon emissions by more than 50%.
-        Deployment of the industry leading VOTRANS™ CNG system enables monetization of these gas
-        resources and stable fuel supply for power projects, where LNG or pipelines are not viable
-        options due to economic, geopolitical or environmental issues.
+        Other than the content you own, under these Terms, EnerSea Transport Inc. and/or its
+        licensors own all the intellectual property rights and materials contained in this Website.
+        You are granted limited license only for purposes of viewing the material contained on this
+        Website.
       </SectionText>
+      <b>Restrictions</b>
       <SectionText>
-        EnerSea’s marine gas transportation services are offered on a long-term tariff basis similar
-        to third-party pipeline transportation agreements. This ensures clients are not burdened
-        with the upfront capital expenditures related to the dedicated CNG ships or terminals.
+        You are specifically restricted from all of the following:
+        <ul>
+          <li>publishing any Website material in any other media;</li>
+          <li>selling, sublicensing and/or otherwise commercializing any Website material;</li>
+          <li>publicly performing and/or showing any Website material;</li>
+          <li>using this Website in any way that is or may be damaging to this Website;</li>
+          <li>using this Website in any way that impacts user access to this Website;</li>
+          <li>
+            using this Website contrary to applicable laws and regulations, or in any way may cause
+            harm to the Website, or to any person or business entity;
+          </li>
+          <li>
+            engaging in any data mining, data harvesting, data extracting or any other similar
+            activity in relation to this Website;
+          </li>
+          <li>using this Website to engage in any advertising or marketing.</li>
+        </ul>
+        Certain areas of this Website are restricted from being access by you and EnerSea Transport
+        Inc. may further restrict access by you to any areas of this Website, at any time, in
+        absolute discretion. Any user ID and password you may have for this Website are confidential
+        and you must maintain confidentiality as well.
+        <br />
+        <br />
+        <b>Your Content </b>
+        <br />
+        <br />
+        In these Website Standard Terms and Conditions, Your Content shall mean any audio, video
+        text, images or other material you choose to display on this Website. By displaying Your
+        Content, you grant EnerSea Transport Inc. a non-exclusive, worldwide irrevocable, sub
+        licensable license to use, reproduce, adapt, publish, translate and distribute it in any and
+        all media. Your Content must be your own and must not be invading any third-party’s rights.
+        EnerSea Transport Inc. reserves the right to remove any of Your Content from this Website at
+        any time without notice.
+        <br />
+        <br />
+        <b>Your Privacy</b>
+        <br />
+        <br />
+        Please read Privacy Policy.
+        <br />
+        <br />
+        <b>No Warranties</b>
+        <br />
+        <br />
+        This Website is provided as is, with all faults, and EnerSea Transport Inc. express no
+        representations or warranties, of any kind related to this Website or the materials
+        contained on this Website. Also, nothing contained on this Website shall be interpreted as
+        advising you.
+        <br />
+        <br />
+        <b>Limitation of Liability</b>
+        <br />
+        <br />
+        In no event shall EnerSea Transport Inc., nor any of its officers, directors and employees,
+        shall be held liable for anything arising out of or in any way connected with your use of
+        this Website whether such liability is under contract. EnerSea Transport Inc., including its
+        officers, directors and employees shall not be held liable for any indirect, consequential
+        or special liability arising out of or in any way related to your use of this Website.
+        <br />
+        <br />
+        <b>Indemnification</b>
+        <br />
+        <br />
+        You hereby indemnify to the fullest extent EnerSea Transport Inc. from and against any
+        and/or all liabilities, costs, demands, causes of action, damages and expenses arising in
+        any way related to your breach of any of the provisions of these Terms.
+        <br />
+        <br />
+        <b>Severability</b>
+        <br />
+        <br />
+        If any provision of these Terms is found to be invalid under any applicable law, such
+        provisions shall be deleted without affecting the remaining provisions herein.
+        <br />
+        <br />
+        <b>Variation of Terms</b>
+        <br />
+        <br />
+        EnerSea Transport Inc. is permitted to revise these Terms at any time as it sees fit, and by
+        using this Website you are expected to review these Terms on a regular basis.
+        <br />
+        <br />
+        <b>Assignment</b>
+        <br />
+        <br />
+        The EnerSea Transport Inc. is allowed to assign, transfer, and subcontract its rights and/or
+        obligations under these Terms without any notification. However, you are not allowed to
+        assign, transfer, or subcontract any of your rights and/or obligations under these Terms.
+        <br />
+        <br />
+        <b>Entire Agreement</b>
+        <br />
+        <br />
+        These Terms constitute the entire agreement between EnerSea Transport Inc. and you in
+        relation to your use of this Website, and supersede all prior agreements and understandings.
+        <br />
+        <br />
+        <b>Governing Law & Jurisdiction</b>
+        <br />
+        <br />
+        These Terms will be governed by and interpreted in accordance with the laws of the State of
+        us, and you submit to the non-exclusive jurisdiction of the state and federal courts located
+        in us for the resolution of any disputes.
+        <br />
+        <br />
       </SectionText>
     </section>
   );
